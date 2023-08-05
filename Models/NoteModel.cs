859189ae4d79a8f6634d604cc050bd61f5679261
Estebanespg <estebanespg1@gmail.com>
2023-08-04 19:23:53 -0500

    Commit with NoteAppShared

--- Models/NoteModel.cs
+++ Models/NoteModel.cs
@@ -1,14 +1,15 @@
 ﻿using NoteApp.Shared.Enums;
+using System.Collections.Generic;
 
 namespace NoteApp.Shared.Models
 {
     public class NoteModel
     {
-        public int Id { get; set; }
+        public int ID { get; set; }
         public int Title { get; set; }
         public string Description { get; set; }
         public byte[] Photo { get; set; }
-        public Categories[] Category { get; set; }
+        public List<Categories> Category { get; set; }
         public States State { get; set; }
         public int UserId { get; set; }
     }
