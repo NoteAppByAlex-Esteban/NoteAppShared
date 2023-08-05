fffaf69276b8a40bb613593ff1df127af190c559
Alex <giraldojhong4@gmail.com>
2023-08-05 03:35:41 -0500

    C# 7 to C# 11

--- Responses/AuthenticationResponse.cs
+++ Responses/AuthenticationResponse.cs
@@ -6,6 +6,7 @@ public class AuthenticationResponse
 
     public string Jwt { get; set; }
     public string Message { get; set; }
+    public Models.UserModel User { get; set; }
     public bool IsSuccess { get; set; }
 
 }
